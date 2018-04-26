2045cf60ef74ad5264d9555c060b8cce6baed17b
Ilya Andreev <ilyaa1982@gmail.com>
2018-04-25 21:32:09 -0400

    updated client.js, css, html

--- public/client.js
+++ public/client.js
@@ -6,3 +6,26 @@ $(document).on("click", "#sidebar-btn", function(event){
     $('#sidebar').toggleClass('visible');
 });
 
+//Results
+function getDataFromNyt(){
+    event.preventDefault();
+    const outcome = $('#sport-section');
+    var url =                       "https://api.nytimes.com/svc/topstories/v2/sports.json";
+    url += '?' + $.param({
+        'api-key': "1b45aab26c4f43869d611a4c1ff2c95d"
+    });
+    $.ajax({
+        url: url,
+        method: 'GET',
+
+    }).done(function(result) {
+        console.log(result);
+        outcome.html("");
+        outcome.append(`<div class="row"><div class="col-4"><span class="title">${results.title}</span><br><br>
+                      <img class="photo" src="${results.multimedia.url}">
+                      <p class="text"><strong>${results.abstract}</strong></p></div></div>`);
+    }).fail(function(err) {
+        throw err;
+    });
+}
+

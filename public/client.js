b963b208cc80142247b2d338220fab6a3091bbe6
Ilya Andreev <ilyaa1982@gmail.com>
2018-04-28 13:52:44 -0400

    updated client.js, html, css

--- public/client.js
+++ public/client.js
@@ -1,9 +1,11 @@
 /*Define variables objects and functions*/
 
 //Open sidebar
-$(document).on("click", "#sidebar-btn", function(event){
+$(document).on("click", "#sidebar-btn", function (event) {
     event.preventDefault();
-    $('#sidebar').toggleClass('visible');
+    $('#sidebar').addClass('visible');
+    $('#sidebar-btn').hide();
+    $('#sidebar-close').show();
 });
 
 /*$(document).on("mouseout", "#sidebar ul li a", function(event){
@@ -12,39 +14,43 @@ $(document).on("click", "#sidebar-btn", function(event){
 });*/
 
 //Close sidebar
-
+$(document).on("click", "#sidebar-close", function (event) {
+    event.preventDefault();
+    $('#sidebar').removeClass('visible');
+    $('#sidebar-btn').show();
+});
 
 //Sidebar menu
-$(document).on("click", "#home", function(event){
+$(document).on("click", "#home", function (event) {
     event.preventDefault();
     location.reload();
 });
 
-$(document).on("click", "#sports", function(event){
+$(document).on("click", "#sports", function (event) {
     event.preventDefault();
     $('.hide-me').hide();
     $('.sport-page').show();
 });
 
-$(document).on("click", "#arts", function(event){
+$(document).on("click", "#arts", function (event) {
     event.preventDefault();
     $('.hide-me').hide();
     $('.art-page').show();
 });
 
-$(document).on("click", "#politics", function(event){
+$(document).on("click", "#politics", function (event) {
     event.preventDefault();
     $('.hide-me').hide();
     $('.politics-page').show();
 });
 
-$(document).on("click", "#business", function(event){
+$(document).on("click", "#business", function (event) {
     event.preventDefault();
     $('.hide-me').hide();
     $('.business-page').show();
 });
 
-$(document).on("click", "#search", function(event){
+$(document).on("click", "#search", function (event) {
     event.preventDefault();
     $('.hide-me').hide();
     $('.login-page').show();
@@ -54,7 +60,7 @@ $(document).on("click", "#search", function(event){
 
 
 //Results
-function getDataFromNyt(){
+function getDataFromNyt() {
     event.preventDefault();
     const outcome = $('#sport-section');
     var url = "https://api.nytimes.com/svc/topstories/v2/sports.json";
@@ -65,16 +71,13 @@ function getDataFromNyt(){
         url: url,
         method: 'GET',
 
-    }).done(function(result) {
+    }).done(function (result) {
         console.log(result);
         outcome.html("");
         outcome.append(`<div class="row"><div class="col-4"><span class="title">${results.title}</span><br><br>
                       <img class="photo" src="${results.multimedia.url}">
                       <p class="text"><strong>${results.abstract}</strong></p></div></div>`);
-    }).fail(function(err) {
+    }).fail(function (err) {
         throw err;
     });
 }
-
-
-
